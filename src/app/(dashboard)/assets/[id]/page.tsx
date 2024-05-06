6490ba1fc9c2bbf34396c96a95e21a73d93b3ca8
Przemek <przemek.lewtak@heywood.co.uk>
2024-05-06 15:13:19 +0100

    Added changes to details page

--- src/app/(dashboard)/assets/[id]/page.tsx
+++ src/app/(dashboard)/assets/[id]/page.tsx
@@ -3,10 +3,28 @@
 import { Title } from "@/components/ui/Title";
 import { useAssetDetails } from "../_hooks/useAssetDetails";
 import { useParams } from "next/navigation";
+import { useContext } from "react";
+import { useAssetsContext } from "@/context/AssetsContext";
+import { Asset } from "@/Data/defs";
 
 export default function Details() {
   const params = useParams<{ id: string }>();
+  const { assets } = useAssetsContext();
   const { singleAsset } = useAssetDetails(Number(params.id));
+
+  const copiedAsstets = structuredClone(assets);
+
+  const sortedData: Asset[] = [];
+
+  const sortData = (asset: Asset) => {
+    const { children } = asset;
+
+    sortedData.push(asset);
+
+    if (children) {
+    }
+  };
+
   console.log(params, "params");
   console.log(singleAsset, "vggfbrt");
   return (
